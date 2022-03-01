86d3ec4dd7ac7313bc4eb8e8b38d709fc5cc12d3
amir <amirgh1380@gmail.com>
2022-03-01 16:13:53 +0330

    initializing style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,57 @@
 const Navbar = () => {
   return (
-    <header>
-      <nav></nav>
+    <header className="nav">
+      <nav>
+        <a className="btn" href="#home">
+          <img alt="twitter-icon" />
+        </a>
+        <li>
+          <i></i>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <i></i>
+          <a href="#explore">Explore</a>
+        </li>
+        <li>
+          <i></i>
+          <a href="#notifications">Notifications</a>
+        </li>
+        <li>
+          <i></i>
+          <a href="#messages">Messages</a>
+        </li>
+        <li>
+          <i></i>
+          <a href="#bookmarks">Bookmarks</a>
+        </li>
+        <li>
+          <i></i>
+          <a href="#lists">Lists</a>
+        </li>
+        <li>
+          <i></i>
+          <a href="#profile">Profile</a>
+        </li>
+        <li>
+          <i></i>
+          <a href="#more">More</a>
+        </li>
+
+        <button>Tweet</button>
+      </nav>
+
+      <div>
+        {/* image */}
+        <div></div>
+        {/* Data */}
+        <div>
+          <h3>Amirhosein Gharaati</h3>
+          <span>@AG_1380</span>
+        </div>
+        {/* icon */}
+        <div></div>
+      </div>
     </header>
   );
 };
