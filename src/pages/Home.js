19a1b02965a531b65c68ce5cfb82dff129f6cc54
amir <amirgh1380@gmail.com>
2022-03-02 21:54:37 +0330

    home textarea

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
           <img src={profileLogo} alt="profile" />
         </div>
         <div className="add__content">
-          <textarea placeholder="What's happening?" className="add__text" />
+          <textarea placeholder="What's happening?" />
           <div className="add__line"></div>
           <div className="add__icons-wrapper">
             <div className="add__icons">
