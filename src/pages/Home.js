5c9faf267e03a24214690dcebe0f6127173026af
amir <amirgh1380@gmail.com>
2022-03-02 23:11:30 +0330

    btn-disabled

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -36,7 +36,7 @@ const Home = () => {
               </button>
             </div>
             <div className="add__button-wrapper">
-              <button className="add__button btn">Tweet</button>
+              <button className="add__button btn btn-disabled">Tweet</button>
             </div>
           </div>
         </div>
