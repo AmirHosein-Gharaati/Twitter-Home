71dfb5a80f8fbe9c9648912c7b309be94bae86f9
amir <amirgh1380@gmail.com>
2022-03-02 21:44:17 +0330

    home add post html structure

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,46 @@
+import logo from "../assets/vectors/logo.svg";
+import profileLogo from "../assets/images/profile.png";
+
 const Home = () => {
   return (
     <div className="home">
       <h3 className="home__title">Home</h3>
+
+      {/* Add post */}
+      <div className="add">
+        <div className="add__proifle">
+          <img src={profileLogo} alt="profile" />
+        </div>
+        <div className="add__content">
+          <textarea placeholder="What's happening?" className="add__text" />
+          <div className="add__line"></div>
+          <div className="add__icons-wrapper">
+            <div className="add__icons">
+              <button className="btn">
+                <img src={logo} alt="logo" />
+              </button>
+              <button className="btn">
+                <img src={logo} alt="logo" />
+              </button>
+              <button className="btn">
+                <img src={logo} alt="logo" />
+              </button>
+              <button className="btn">
+                <img src={logo} alt="logo" />
+              </button>
+              <button className="btn">
+                <img src={logo} alt="logo" />
+              </button>
+              <button className="btn">
+                <img src={logo} alt="logo" />
+              </button>
+            </div>
+            <div className="add__button-wrapper">
+              <button className="add__button">Tweet</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
