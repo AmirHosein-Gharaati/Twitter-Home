6c96f5320dd288b14a32ec45d6f6936ae069f509
amir <amirgh1380@gmail.com>
2022-03-03 23:34:32 +0330

    changed navbar icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,72 +2,90 @@ import logo from "../assets/vectors/logo.svg";
 import profileLogo from "../assets/images/profile.png";
 import dots from "../assets/images/dots.png";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHome,
+  faHashtag,
+  faBell,
+  faEnvelope,
+  faNoteSticky,
+  faList,
+  faPerson,
+  faDotCircle,
+  faFeather,
+} from "@fortawesome/free-solid-svg-icons";
+
 const Navbar = () => {
   return (
     <div className="nav">
-      {/* <button className="btn nav__icon" href="#home">
-        <img src={logo} alt="twitter-icon" />
-      </button> */}
+      <div>
+        <button className="btn nav__icon" href="#home">
+          <img src={logo} alt="twitter-icon" />
+        </button>
 
-      <nav className="navbar">
-        <li>
-          <div className="navbar__item navbar__active btn">
-            <img src={logo} alt="logo" />
-            <a href="#home">Home</a>
-          </div>
-        </li>
+        <nav className="navbar">
+          <li>
+            <div className="navbar__item navbar__active btn">
+              <FontAwesomeIcon icon={faHome} />
+              <a href="#home">Home</a>
+            </div>
+          </li>
 
-        <li>
-          <div className="navbar__item btn">
-            <img src={logo} alt="logo" />
-            <a href="#explore">explore</a>
-          </div>
-        </li>
+          <li>
+            <div className="navbar__item btn">
+              <FontAwesomeIcon icon={faHashtag} />
+              <a href="#explore">explore</a>
+            </div>
+          </li>
 
-        <li>
-          <div className="navbar__item btn">
-            <img src={logo} alt="logo" />
-            <a href="#notifications">notifications</a>
-          </div>
-        </li>
+          <li>
+            <div className="navbar__item btn">
+              <FontAwesomeIcon icon={faBell} />
+              <a href="#notifications">notifications</a>
+            </div>
+          </li>
 
-        <li>
-          <div className="navbar__item btn">
-            <img src={logo} alt="logo" />
-            <a href="#messages">messages</a>
-          </div>
-        </li>
+          <li>
+            <div className="navbar__item btn">
+              <FontAwesomeIcon icon={faEnvelope} />
+              <a href="#messages">messages</a>
+            </div>
+          </li>
 
-        <li>
-          <div className="navbar__item btn">
-            <img src={logo} alt="logo" />
-            <a href="#bookmarks">bookmarks</a>
-          </div>
-        </li>
+          <li>
+            <div className="navbar__item btn">
+              <FontAwesomeIcon icon={faNoteSticky} />
+              <a href="#bookmarks">bookmarks</a>
+            </div>
+          </li>
 
-        <li>
-          <div className="navbar__item btn">
-            <img src={logo} alt="logo" />
-            <a href="#lists">lists</a>
-          </div>
-        </li>
+          <li>
+            <div className="navbar__item btn">
+              <FontAwesomeIcon icon={faList} />
+              <a href="#lists">lists</a>
+            </div>
+          </li>
 
-        <li>
-          <div className="navbar__item btn">
-            <img src={logo} alt="logo" />
-            <a href="#profile">profile</a>
-          </div>
-        </li>
+          <li>
+            <div className="navbar__item btn">
+              <FontAwesomeIcon icon={faPerson} />
+              <a href="#profile">profile</a>
+            </div>
+          </li>
 
-        <li>
-          <div className="navbar__item btn">
-            <img src={logo} alt="logo" />
-            <a href="#more">more</a>
-          </div>
-        </li>
+          <li>
+            <div className="navbar__item btn">
+              <FontAwesomeIcon icon={faDotCircle} />
+              <a href="#more">more</a>
+            </div>
+          </li>
 
-        <button className="btn navbar__tweet">Tweet</button>
-      </nav>
+          <button className="btn navbar__tweet">
+            <span>Tweet</span>
+            <FontAwesomeIcon className="navbar__tweet__icon" icon={faFeather} />
+          </button>
+        </nav>
+      </div>
 
       <div className="profile btn">
         {/* image */}
