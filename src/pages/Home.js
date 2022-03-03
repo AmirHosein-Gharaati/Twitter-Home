a395d9bfdf46ab7a6b86642f325e172d0d0fd817
amir <amirgh1380@gmail.com>
2022-03-03 11:54:34 +0330

    post image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -42,17 +42,15 @@ const Home = () => {
                 by the readable content of a page when looking at its layout.
                 The point of using Lorem Ipsum is that it has a more-or-less
                 normal distribution of letters, as opposed to using 'Content
-                here, content here', making it look like readable English. Many
-                desktop publishing packages and web page editors now use Lorem
-                Ipsum as their default model text, and a search for 'lorem
-                ipsum' will uncover many web sites still in their infancy.
-                Various versions have evolved over the years, sometimes by
-                accident, sometimes on purpose (injected humour and the like).
+                here, content here', making it look like readable English.
               </p>
             </div>
 
             <div className="post__image">
-              <img src={profileLogo} alt="logo" />
+              <img
+                src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+                alt="logo"
+              />
             </div>
 
             <div className="post__buttons">
