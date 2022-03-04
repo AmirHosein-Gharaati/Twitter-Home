cf03204c633d5822ba6e61f893b6f94b8bd7bc5c
amir <amirgh1380@gmail.com>
2022-03-04 19:58:52 +0330

    styling right-side (has bug)

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ function App() {
 
         {/* Right Side */}
         <div className="right-side">
-
           <div className="right-side__search-wrapper">
             <div className="right-side__search">
               <input placeholder="Search Twitter" />
@@ -22,79 +21,84 @@ function App() {
             </div>
           </div>
 
-          <div className="right-side__content">
+          <div className="right-side__box">
+            <h2 className="right-side__box__title">What's happening</h2>
 
-            <div className="right-side__box">
-              <h2 className="right-side__box__title">What's happening</h2>
-
-              <div className="right-side__item">
-                <div className="right-side__item__content">
-                  <span className="right-side__item__title">
-                    Ukraine conflict · LIVE
-                  </span>
-                  <p>
-                    UN Human Rights Council votes to investigate 'Russian
-                    aggression against Ukraine'
-                  </p>
-                </div>
-                <div className="right-side__item__image">
-                  <img
-                    src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
-                    alt=""
-                  />
-                </div>
+            <div className="right-side__item">
+              <div className="right-side__item__content">
+                <span className="right-side__item__title">
+                  Ukraine conflict · LIVE
+                </span>
+                <p>
+                  UN Human Rights Council votes to investigate 'Russian
+                  aggression against Ukraine'
+                </p>
               </div>
-
-              <div className="right-side__item">
-                <div className="right-side__item__content">
-                  <span className="right-side__item__title">
-                    Ukraine conflict · LIVE
-                  </span>
-                  <p>
-                    UN Human Rights Council votes to investigate 'Russian
-                    aggression against Ukraine'
-                  </p>
-                </div>
-                <div className="right-side__item__image">
-                  <img
-                    src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
-                    alt=""
-                  />
-                </div>
+              <div className="right-side__item__image">
+                <img
+                  src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+                  alt=""
+                />
               </div>
-
             </div>
 
-            {/* <div className="right-side__box">
-              <h2 className="right-side__box__title">Who to follow</h2>
-
-              <div className="right-side__item">
-                <div className="right-side__item__content">
-                  <div className="right-side__item__image rounded">
-                    <img
-                      src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
-                      alt=""
-                    />
-                  </div>
-                  <h4 className="right-side__item__name">Sam</h4>
-                  <span className="right-side__item__id">@samasadii</span>
-                </div>
+            <div className="right-side__item">
+              <div className="right-side__item__content">
+                <span className="right-side__item__title">
+                  Ukraine conflict · LIVE
+                </span>
+                <p>
+                  UN Human Rights Council votes to investigate 'Russian
+                  aggression against Ukraine'
+                </p>
               </div>
-
-              <div className="right-side__item">
-                <div className="right-side__item__content">
-                  <div className="right-side__item__image rounded">
-                    <img
-                      src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
-                      alt=""
-                    />
-                  </div>
-                  <h4 className="right-side__item__name">Sam</h4>
-                  <span className="right-side__item__id">@samasadii</span>
-                </div>
+              <div className="right-side__item__image">
+                <img
+                  src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+                  alt=""
+                />
               </div>
+            </div>
+          </div>
 
-            </div> */}
+          <div className="right-side__box">
+            <h2 className="right-side__box__title">What's happening</h2>
+
+            <div className="right-side__item">
+              <div className="right-side__item__content">
+                <span className="right-side__item__title">
+                  Ukraine conflict · LIVE
+                </span>
+                <p>
+                  UN Human Rights Council votes to investigate 'Russian
+                  aggression against Ukraine'
+                </p>
+              </div>
+              <div className="right-side__item__image">
+                <img
+                  src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+                  alt=""
+                />
+              </div>
+            </div>
+
+            <div className="right-side__item">
+              <div className="right-side__item__content">
+                <span className="right-side__item__title">
+                  Ukraine conflict · LIVE
+                </span>
+                <p>
+                  UN Human Rights Council votes to investigate 'Russian
+                  aggression against Ukraine'
+                </p>
+              </div>
+              <div className="right-side__item__image">
+                <img
+                  src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+                  alt=""
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
