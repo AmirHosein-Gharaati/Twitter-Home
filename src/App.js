0cc74bca3c36ce94b51a8e9a735c6f78366fad88
amir <amirgh1380@gmail.com>
2022-03-04 17:01:32 +0330

    right side initial

--- src/App.js
+++ src/App.js
@@ -7,7 +7,68 @@ function App() {
       <Navbar />
       <div className="main">
         <Home />
-        <div className="right-side"></div>
+        <div className="right-side">
+          <div className="right-side__search">
+            <textarea placeholder="Search Twitter" />
+            <div className="right-side__search__results"></div>
+          </div>
+          {/* <div className="right-side__content">
+
+            <div className="right-side__box">
+              <h2 className="right-side__box__title">What's happening</h2>
+
+              <div className="right-side__item">
+                <div className="right-side__item__content">
+                  <span className="right-side__item__id">
+                    Ukraine conflict · LIVE
+                  </span>
+                  <p>
+                    UN Human Rights Council votes to investigate 'Russian
+                    aggression against Ukraine'
+                  </p>
+                </div>
+                <div className="right-side__item__image">
+                  <img
+                    src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+                    alt=""
+                  />
+                </div>
+              </div>
+
+            </div>
+
+            <div className="right-side__box">
+              <h2 className="right-side__box__title">Who to follow</h2>
+
+              <div className="right-side__item">
+                <div className="right-side__item__content">
+                  <div className="right-side__item__image rounded">
+                    <img
+                      src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+                      alt=""
+                    />
+                  </div>
+                  <h4 className="right-side__item__name">Sam</h4>
+                  <span className="right-side__item__id">@samasadii</span>
+                </div>
+              </div>
+
+              <div className="right-side__item">
+                <div className="right-side__item__content">
+                  <div className="right-side__item__image rounded">
+                    <img
+                      src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+                      alt=""
+                    />
+                  </div>
+                  <h4 className="right-side__item__name">Sam</h4>
+                  <span className="right-side__item__id">@samasadii</span>
+                </div>
+              </div>
+
+            </div>
+          </div> */}
+        </div>
       </div>
     </div>
   );
