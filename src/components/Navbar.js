aa4adf444522a0aa9b4d7aacd259e6600a1b0f27
amir <amirgh1380@gmail.com>
2022-03-05 13:26:16 +0330

    fix navbar in mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -26,49 +26,57 @@ const Navbar = () => {
         <nav className="navbar">
           <li>
             <a href="#home" className="navbar__item navbar__active btn">
-              <FontAwesomeIcon icon={faHome} /> Home
+              <FontAwesomeIcon icon={faHome} />
+              <span className="navbar__item__text">home</span>
             </a>
           </li>
 
           <li>
             <a href="#explore" className="navbar__item  btn">
-              <FontAwesomeIcon icon={faHashtag} /> explore
+              <FontAwesomeIcon icon={faHashtag} />
+              <span className="navbar__item__text">explore</span>
             </a>
           </li>
 
           <li>
             <a href="#notifications" className="navbar__item  btn">
-              <FontAwesomeIcon icon={faBell} /> notifications
+              <FontAwesomeIcon icon={faBell} />
+              <span className="navbar__item__text">notifications</span>
             </a>
           </li>
 
           <li>
             <a href="#messages" className="navbar__item  btn">
-              <FontAwesomeIcon icon={faEnvelope} /> messages
+              <FontAwesomeIcon icon={faEnvelope} />
+              <span className="navbar__item__text">messages</span>
             </a>
           </li>
 
           <li>
             <a href="#bookmarks" className="navbar__item  btn">
-              <FontAwesomeIcon icon={faNoteSticky} /> bookmarks
+              <FontAwesomeIcon icon={faNoteSticky} />
+              <span className="navbar__item__text">bookmarks</span>
             </a>
           </li>
 
           <li>
             <a href="#lists" className="navbar__item  btn">
-              <FontAwesomeIcon icon={faList} /> lists
+              <FontAwesomeIcon icon={faList} />
+              <span className="navbar__item__text">lists</span>
             </a>
           </li>
 
           <li>
             <a href="#profile" className="navbar__item  btn">
-              <FontAwesomeIcon icon={faPerson} /> profile
+              <FontAwesomeIcon icon={faPerson} />
+              <span className="navbar__item__text">profile</span>
             </a>
           </li>
 
           <li>
             <a href="#more" className="navbar__item  btn">
-              <FontAwesomeIcon icon={faDotCircle} /> more
+              <FontAwesomeIcon icon={faDotCircle} />
+              <span className="navbar__item__text">more</span>
             </a>
           </li>
 
